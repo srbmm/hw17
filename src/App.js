786c4bc7f5ceb5ab4f0272007f6653b7cba5b3b0
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-01-13 08:14:02 +0330

    make fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const [counter, setCounter] = useState(0);
   return (
     <div className="container">
         <div className="app">
-            <button className="minus" onClick={() => setCounter(counter-1)}>-</button>
+            <button className="minus" onClick={() => setCounter(counter > 0 ? counter-1:counter)}>-</button>
             <div className="">{counter}</div>
             <button className="plus" onClick={() => setCounter(counter+1)}>+</button>
         </div>

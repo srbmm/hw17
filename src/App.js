c60ac53deb4c704e45cc849afee449489495a5db
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-01-13 08:17:47 +0330

    added hello react.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 const [counter, setCounter] = useState(0);
   return (
     <div className="container">
+        <h1>Hello react!</h1>
         <div className="app">
             <button className="minus" onClick={() => setCounter(counter > 0 ? counter-1:counter)}>-</button>
             <div className="">{counter}</div>
